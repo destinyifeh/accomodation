1ff533144d20b2ef7a37757a2d53087c026b20c5
destiny <destinyifeh@gmail.com>
2022-08-21 03:05:29 -1200

    updated

--- services/agents/api.js
+++ services/agents/api.js
@@ -1,27 +1,55 @@
 import axios from "axios";
 import { base_url } from "../requesters";
-export const getAgents = axios.get(`${base_url}/api/agent`);
+export const getAgents = axios.get(`${base_url}/api/agent`, {
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
 
 export function sendForgotEmail(mail) {
-  return axios.post("/api/agent/forgot-password", mail);
+  return axios.post("/api/agent/forgot-password", mail, {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
 }
 
 export function sendForgotCode(theCode) {
-  return axios.post("/api/agent/reset-code", theCode);
+  return axios.post("/api/agent/reset-code", theCode, {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
 }
 
 export function sendReset(agentId, newPassword) {
-  return axios.post(`/api/agent/reset/${agentId}`, newPassword);
+  return axios.post(`/api/agent/reset/${agentId}`, newPassword, {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
 }
 
 export function sendRegister(formData) {
-  return axios.post("/api/agent/register", formData);
+  return axios.post("/api/agent/register", formData, {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
 }
 
 export function sendLogin(formData) {
-  return axios.post("/api/agent/login", formData);
+  return axios.post("/api/agent/login", formData, {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
 }
 
 export function getAgent(id) {
-  return axios.get(`${base_url}/api/agent/${id}`);
+  return axios.get(`${base_url}/api/agent/${id}`, {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
 }
--- services/property/api.js
+++ services/property/api.js
@@ -5,33 +5,65 @@ export function addNewProperty(data) {
 }
 
 export function getProperties() {
-  return axios.get(`${base_url}/api/property/new`);
+  return axios.get(`${base_url}/api/property/new`, {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
 }
 
 export function getRecent() {
-  return axios.get(`${base_url}/api/property/new`);
+  return axios.get(`${base_url}/api/property/new`, {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
 }
 
 export function getProperty(id) {
-  return axios.get(`${base_url}/api/property/${id}`);
+  return axios.get(`${base_url}/api/property/${id}`, {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
 }
 
 export function deleteProperty(id) {
-  return axios.delete(`${base_url}/api/property/${id}`);
+  return axios.delete(`${base_url}/api/property/${id}`, {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
 }
 
 export function updatePropertyRequest({ id, data }) {
-  return axios.put(`/api/property/${id}`, data);
+  return axios.put(`/api/property/${id}`, data, {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
 }
 
 export function getRelatedProperty(slug) {
-  return axios.get(`${base_url}/api/property/related/${slug}`);
+  return axios.get(`${base_url}/api/property/related/${slug}`, {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
 }
 
 export function getPropertySlug(slug) {
-  return axios.get(`${base_url}/api/property/main/${slug}`);
+  return axios.get(`${base_url}/api/property/main/${slug}`, {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
 }
 
 export function getQuery(slug) {
-  return axios.get(`${base_url}/api/property/query/${slug}`);
+  return axios.get(`${base_url}/api/property/query/${slug}`, {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
 }
