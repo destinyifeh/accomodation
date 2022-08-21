d20ca4f48dce8464293021c8e2d10c39a1fd6492
destiny <destinyifeh@gmail.com>
2022-08-21 03:34:15 -1200

    updated

--- services/property/api.js
+++ services/property/api.js
@@ -1,73 +1,37 @@
 import axios from "axios";
 import { base_url } from "../requesters";
 export function addNewProperty(data) {
-  return axios.post("/api/property/new", data, {
-    headers: {
-      Accept: "application/json",
-    },
-  });
+  return axios.post("/api/property/new", data);
 }
 
 export function getProperties() {
-  return axios.get(`${base_url}/api/property/new`, {
-    headers: {
-      Accept: "application/json",
-    },
-  });
+  return axios.get(`${base_url}/api/property/new`);
 }
 
 export function getRecent() {
-  return axios.get(`${base_url}/api/property/new`, {
-    headers: {
-      Accept: "application/json",
-    },
-  });
+  return axios.get(`${base_url}/api/property/new`);
 }
 
 export function getProperty(id) {
-  return axios.get(`${base_url}/api/property/${id}`, {
-    headers: {
-      Accept: "application/json",
-    },
-  });
+  return axios.get(`${base_url}/api/property/${id}`);
 }
 
 export function deleteProperty(id) {
-  return axios.delete(`${base_url}/api/property/${id}`, {
-    headers: {
-      Accept: "application/json",
-    },
-  });
+  return axios.delete(`${base_url}/api/property/${id}`);
 }
 
 export function updatePropertyRequest({ id, data }) {
-  return axios.put(`/api/property/${id}`, data, {
-    headers: {
-      Accept: "application/json",
-    },
-  });
+  return axios.put(`/api/property/${id}`, data);
 }
 
 export function getRelatedProperty(slug) {
-  return axios.get(`${base_url}/api/property/related/${slug}`, {
-    headers: {
-      Accept: "application/json",
-    },
-  });
+  return axios.get(`${base_url}/api/property/related/${slug}`);
 }
 
 export function getPropertySlug(slug) {
-  return axios.get(`${base_url}/api/property/main/${slug}`, {
-    headers: {
-      Accept: "application/json",
-    },
-  });
+  return axios.get(`${base_url}/api/property/main/${slug}`);
 }
 
 export function getQuery(slug) {
-  return axios.get(`${base_url}/api/property/query/${slug}`, {
-    headers: {
-      Accept: "application/json",
-    },
-  });
+  return axios.get(`${base_url}/api/property/query/${slug}`);
 }
